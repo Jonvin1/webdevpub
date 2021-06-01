f5ece2b09482803d2f1c68955e677853abf990f0
Jonvin1 <jonvin@edu.umea.se>
2021-06-01 12:10:53 +0200

    changed @media and finalized some things

--- js/base.js
+++ js/base.js
@@ -2,16 +2,16 @@ let pictures = ["recources/bild1.png", "recources/bild2.png", "recources/bild3.p
 let i = 0;
 document.getElementById("btnNext").addEventListener("click", nextImg)
 document.getElementById("btnPrev").addEventListener("click", prevImg)
-let imgSrc = document.getElementById("img").src = pictures[0];
+document.getElementById("img").src = pictures[0];
 
 function slideShow() {
     i++
     if (i < pictures.length) {
-        imgSrc = pictures[i];
+        document.getElementById("img").src = pictures[i];
         setTimeout(slideShow, 3000);
     } else {
         i = 0;
-        imgSrc = pictures[i];
+        document.getElementById("img").src = pictures[i];
         setTimeout(slideShow, 3000);
     }
 }
@@ -19,19 +19,19 @@ function slideShow() {
 function nextImg(){
     i++;
     if (i < pictures.length) {
-        imgSrc = pictures[i];
+        document.getElementById("img").src = pictures[i];
     } else {
         i = 0;
-        imgSrc = pictures[i];
+        document.getElementById("img").src = pictures[i];
     }
 }
 function prevImg(){
     i--;
     if(i > -1){
-        imgSrc = pictures[i];
+        document.getElementById("img").src = pictures[i];
     } else{
         i = pictures.length - 1;
-        imgSrc = pictures[i];
+        document.getElementById("img").src = pictures[i];
     }
 }
 
